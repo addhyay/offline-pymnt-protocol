3b41170ada12e73337110bc8b614d07f5981cf68
Harish V Chavan <addhyay@Harishs-Mac-mini.local>
2025-07-23 16:37:51 +0530

    seperated the modules

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,7 @@
-mod cryptography {
-    use libsecp256k1::{PublicKey, SecretKey};
-    use rand::rngs::OsRng;
-    use sha3::{Digest, Keccak256};
+// modules
+mod cryptography;
 
-    // this struct will create asymmetric key pair over standard secp256k1 curve
-    pub struct KeyPair {
-        pub public_key: PublicKey,
-        _secret_key: SecretKey,
-    }
-
-    impl KeyPair {
-        pub fn new() -> Self {
-            let mut r = OsRng;
-            let _secret_key = SecretKey::random(&mut r);
-            let public_key = PublicKey::from_secret_key(&_secret_key);
-            KeyPair {
-                public_key,
-                _secret_key,
-            }
-        }
-
-        // gets the private key
-        fn _get_secret_key(&self) -> &SecretKey {
-            &self._secret_key
-        }
-    }
-
-    // generates the Ethereum address from public key of treasury
-    pub fn generate_address(public_key: PublicKey) -> String {
-        let hash = Keccak256::digest(&public_key.serialize()[1..]);
-        String::from("0x") + hex::encode(&hash[12..]).as_str()
-    }
-}
-
+// imports
 use crate::cryptography::{generate_address, KeyPair};
 use libsecp256k1::PublicKey;
 
