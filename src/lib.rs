2e0a1244d9450545377d581c15a4d0934cfa6281
Harish V Chavan <addhyay@Harishs-Mac-mini.local>
2025-07-23 16:26:06 +0530

    created mod cryptography for handling all crypto related stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,49 @@
-use libsecp256k1::{PublicKey, SecretKey};
-use rand::rngs::OsRng;
-use sha3::{Digest, Keccak256};
+mod cryptography {
+    use libsecp256k1::{PublicKey, SecretKey};
+    use rand::rngs::OsRng;
+    use sha3::{Digest, Keccak256};
 
-pub struct KeyPair {
-    pub public_key: PublicKey,
-    // this field is kept private so that only enclve functions can access it
-    secret_key: SecretKey,
-}
+    // this struct will create asymmetric key pair over standard secp256k1 curve
+    pub struct KeyPair {
+        pub public_key: PublicKey,
+        _secret_key: SecretKey,
+    }
 
-impl KeyPair {
-    fn new() -> Self {
-        let mut r = OsRng;
-        let secret_key = SecretKey::random(&mut r);
-        let public_key = PublicKey::from_secret_key(&secret_key);
-        KeyPair {
-            public_key,
-            secret_key,
+    impl KeyPair {
+        pub fn new() -> Self {
+            let mut r = OsRng;
+            let _secret_key = SecretKey::random(&mut r);
+            let public_key = PublicKey::from_secret_key(&_secret_key);
+            KeyPair {
+                public_key,
+                _secret_key,
+            }
+        }
+
+        // gets the private key
+        fn _get_secret_key(&self) -> &SecretKey {
+            &self._secret_key
         }
     }
 
-    // this function generates a reference to the secret key and gives to the caller
-    // "supposed to be secure"
-    fn get_secret_key(&self) -> &SecretKey {
-        &self.secret_key
+    // generates the Ethereum address from public key of treasury
+    pub fn generate_address(public_key: PublicKey) -> String {
+        let hash = Keccak256::digest(&public_key.serialize()[1..]);
+        String::from("0x") + hex::encode(&hash[12..]).as_str()
     }
 }
 
+use crate::cryptography::{generate_address, KeyPair};
+use libsecp256k1::PublicKey;
+
 pub fn setup() -> (PublicKey, PublicKey, String) {
-    // generatinf the key pair for enclave and the local-account
+    // generating the key pair for enclave and the local-account
     let manufacturers_key_pair = KeyPair::new();
     let local_key_pair = KeyPair::new();
-
-    // generating the Ethereum address for the treasury
-    let serialize = local_key_pair.public_key.serialize();
-    let serial_public_key_bytes = &serialize[1..];
-    let hash = Keccak256::digest(serial_public_key_bytes);
-    let address = hex::encode(&hash[12..]);
-
-    let _msk = manufacturers_key_pair.get_secret_key();
-    let _sk = local_key_pair.get_secret_key();
 
     (
         manufacturers_key_pair.public_key,
         local_key_pair.public_key,
-        address,
+        generate_address(local_key_pair.public_key),
     )
 }
