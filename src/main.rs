096af19e08bbcae7b55e269805c5a9e4ae9fff13
Harish V Chavan <addhyay@Harishs-Mac-mini.local>
2025-07-23 15:45:39 +0530

    receives an address in String form

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,5 @@ use offline_pymnt_protocol::setup;
 
 fn main() {
     println!("Initiating...");
-    let (pk, m_public_key) = setup();
-
-    println!("mpk = {:?} and pk = {:?}", m_public_key, pk);
+    let (pk, m_public_key, local_address) = setup();
 }
